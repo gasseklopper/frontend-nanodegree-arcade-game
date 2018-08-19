54186c335fb59587feb9dc6b48e6a7f2e1722c4a
gasseklopper <m-morley@gmx.de>
2018-08-19 10:12:01 +0200

    fixed existing starter code
    added missing variablkes for the engine.js to work and added comment for variables to instance the objects of this app

--- js/app.js
+++ js/app.js
@@ -21,16 +21,42 @@ Enemy.prototype.render = function() {
     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
 };
 
+
 // Now write your own player class
 // This class requires an update(), render() and
 // a handleInput() method.
+var Player = function() {
+    // Variables applied to each of our instances go here,
+    // we've provided one for you to get started
 
+    // The image/sprite for our enemies, this uses
+    // a helper we've provided to easily load images
+    this.sprite = 'images/char-boy.png';
+};
+
+// Update the enemy's position, required method for game
+// Parameter: dt, a time delta between ticks
+Player.prototype.update = function() {
+    // You should multiply any movement by the dt parameter
+    // which will ensure the game runs at the same speed for
+    // all computers.
+};
+
+// Draw the enemy on the screen, required method for game
+Player.prototype.render = function() {
+    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+};
 
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
 // Place the player object in a variable called player
 
-
+// array allEnemies for storing all enemy objects
+var allEnemies = []
+// var player that invokes an new Player object referenced by var player
+var player = new Player();
+// push property of enemy object to array allEnemies
+allEnemies.push(new Enemy());
 
 // This listens for key presses and sends the keys to your
 // Player.handleInput() method. You don't need to modify this.
